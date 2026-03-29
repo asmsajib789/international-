3d12762a032a5804ae7246bd22b92a5987b133fb
Sajib <107872992+asmsajib789@users.noreply.github.com>
2026-03-29 11:17:33 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    document.getElementById("registerPopup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("registerPopup").style.display = "none";
+}
