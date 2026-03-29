c27fdaa0f29beb030d9c8aeedeb32121050f3161
Sajib <107872992+asmsajib789@users.noreply.github.com>
2026-03-29 11:21:18 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openPopup() {
-    document.getElementById("registerPopup").style.display = "block";
-}
-
-function closePopup() {
-    document.getElementById("registerPopup").style.display = "none";
-}
